e723074a9e4a43d14f81775dc0ecef7aeb31b305
Stilledood <74854275+Stilledood@users.noreply.github.com>
2022-06-08 15:28:00 +0300

    Added Article Model class

--- Dinamo/agregator/models.py
+++ Dinamo/agregator/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.shortcuts import redirect,render,get_object_or_404,reverse
 
-# Create your models here.
+
+
+class Article(models.Model):
+    '''Class to create a model for all news article'''
+
+    title=models.CharField(max_length=256)
+    image=models.ImageField(upload_to='news_images')
+    description=models.TextField()
+    publishing_site=models.CharField(max_length=128)
+    publishind_date=models.DateTimeField()
+    link=models.URLField(max_length=256)
+
+    class Meta:
+        ordering=['-publishing_date','title']
+        get_latest_by='publishing_date'
+
+    def __str__(self):
+        return sel.title[:50]
+
+    def get_absolute_url(self):
+        return reverse('article_details',kwargs={'pk':self.pk})
+
+    def get_update_url(self):
+        return reverse('article_update',kwargs={'pk':self.pk})
+
+    def get_delete_url(self):
+        return reverse('article_delete',kwargs={'pk':self.pk})
+
+
+
+
+
