f652f091fdb6f8b269e36f51e68a811e6be3db7c
Stilledood <74854275+Stilledood@users.noreply.github.com>
2022-06-14 18:10:32 +0300

    Added Forum app Models for Topics Comments

---
+++ Dinamo/forum/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.shortcuts import reverse
+from django.conf import settings
+
+class Topics(models.Model):
+    '''Class to construct a model for admins added topics'''
+
+    title=models.CharField(max_length=256)
+    date_added=models.DateField(auto_now_add=True)
+    posted_by=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+
+    class Meta:
+        ordering=['-date_added','title']
+        get_latest_by='date_added'
+
+    def __str__(self):
+        if len(self.title) > 30:
+            return self.title[:30]
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('topic_details',kwargs={'pk':self.pk})
+
+    def get_update_url(self):
+        return reverse('topic_update',kwargs={'pk':self.pk})
+
+    def get_delete_url(self):
+        return reverse('topic_delete',kwargs={'pk':self.pk})
+
+
+class Comment(models.Model):
+    '''Class to construct a model for topic comments'''
+
+    body=models.TextField()
+    author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    date_added=models.DateField(auto_now_add=True)
+    topic=models.ForeignKey(Topics,on_delete=models.CASCADE)
+
+    class Meta:
+        ordering=['-date_added']
+        get_latest_by='date_added'
+
+    def __str__(self):
+        if len(self.body)>30:
+            return self.body[:30]
+        return self.body
+
+    def get_update_url(self):
+        return reverse('comment_update',kwargs={'pk':self.pk})
+
+    def get_delete_url(self):
+        return reverse('comment_delete',kwargs={'pk':self.pk})
+
+    
+
