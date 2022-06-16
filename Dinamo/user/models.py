640fda047bcc19c5aa7a639bd797507ae5cc885b
Stilledood <74854275+Stilledood@users.noreply.github.com>
2022-06-16 09:30:01 +0300

    Added Model for User Profile

--- Dinamo/user/models.py
+++ Dinamo/user/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.shortcuts import reverse,render
+from django.contrib.auth.models import User,PermissionsMixin,BaseUserManager
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+class Profile(models.Model):
+    '''Class to construct am model for user profile'''
+
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    image=models.ImageField(upload_to='user_images',default='Dinamo-Logo.jpg')
+    name=models.CharField(max_length=128)
+    username=models.CharField(max_length=32)
+    email_confirmed=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
+
+    def get_absolute_url(self):
+        return reverse('profile_details',kwargs={'username':self.username})
+
+    def get_update_url(self):
+        return reverse('profile_update',kwargs={'username':self.username})
+
+    
