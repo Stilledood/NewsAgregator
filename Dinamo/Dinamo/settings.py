73f3592e684d326701da1c744074314fc1231bc7
Stilledood <74854275+Stilledood@users.noreply.github.com>
2022-06-22 18:02:55 +0300

    Deploy settings files

--- Dinamo/Dinamo/settings.py
+++ Dinamo/Dinamo/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-8sj&*+kce%^)1wt+d4-o%ug%aghembn-zd+hn+kzyh^v#)6sab
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1' 'dinamonews.herokuapp.com']
 
 
 # Application definition
