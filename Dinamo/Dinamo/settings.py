a56989721eed74b4f070716021819a87ec16e7c7
Stilledood <74854275+Stilledood@users.noreply.github.com>
2022-06-23 17:35:07 +0300

    Deploy settings files

--- Dinamo/Dinamo/settings.py
+++ Dinamo/Dinamo/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS=[
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 MEDIA_URL='media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 SITE_ID=1
 
 # Default primary key field type
