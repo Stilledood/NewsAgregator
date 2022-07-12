c946551dd080f77ad8cda2ef8b6347d13314eb09
Florin <74854275+Stilledood@users.noreply.github.com>
2022-07-12 17:47:40 +0300

    Deploy settings files

--- Dinamo/Dinamo/settings.py
+++ Dinamo/Dinamo/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8sj&*+kce%^)1wt+d4-o%ug%aghembn-zd+hn+kzyh^v#)6sab'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
